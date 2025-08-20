e38dbd9a57610db8ff568a3bb84e3b7f5a02816d
Tariqul911 <zxtariqul911@gmail.com>
2025-08-20 09:40:59 +0800

    added key pressed and check right or wrong key pressed

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -31,7 +31,28 @@
 // function showElementById (elementId){
 //     const element = document.getElementById(elementId);
 //     element.classList.remove('hidden');
-// }
+
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key; 
+    console.log( 'player pressed', playerPressed); 
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText; 
+    const expectedAlphabet = currentAlphabet.toLowerCase(); 
+    console.log(playerPressed, expectedAlphabet); 
+
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+    }
+    else{
+        console.log('you missed. you lost a life.');
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 function continueGame(){
     // generate a random alphabet 
